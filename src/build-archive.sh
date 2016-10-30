cfdc0cac0b058de26acbacec3a481cf5b681b771
ivan demakov <ivan@demakov.net>
2016-10-30 19:37:02 +0700

    added libarchive

---
+++ src/build-archive.sh
@@ -0,0 +1,11 @@
+cd libarchive-3.2.1
+
+PATH=/home/win64/bin:$PATH
+export PATH
+
+./configure --prefix=/home/win64 --host=x86_64-w64-mingw32 \
+  --enable-shared --disable-static \
+  CPPFLAGS="-I/home/win64/include" LDFLAGS="-L/home/win64/bin -L/home/win64/lib"
+
+make
+make install
