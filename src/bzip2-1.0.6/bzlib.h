b15e2b12def48706a0327e31c8c8d9ac416ddde6
ivan <ivan@demakov.net>
2017-04-03 00:48:03 +0400

    added zlib prohect

--- src/bzip2-1.0.6/bzlib.h
+++ src/bzip2-1.0.6/bzlib.h
@@ -67,7 +67,7 @@ typedef
 
 
 #ifndef BZ_IMPORT
-//#define BZ_EXPORT
+#define BZ_EXPORT
 #endif
 
 #ifndef BZ_NO_STDIO
@@ -82,12 +82,12 @@ typedef
 #      undef small
 #   endif
 #   ifdef BZ_EXPORT
+#   define BZ_EXTERN __declspec(dllimport)
 #   define BZ_API(func) WINAPI func
-#   define BZ_EXTERN __declspec(dllexport)
 #   else
    /* import windows dll dynamically */
 #   define BZ_API(func) WINAPI func
-#   define BZ_EXTERN __declspec(dllimport)
+#   define BZ_EXTERN __declspec(dllexport)
 #   endif
 #else
 #   define BZ_API(func) func
